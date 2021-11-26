c56dd193433da696f1acab61f0b2b715f3c2ec9d
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2021-11-26 13:25:54 +0000

    Create train.py

---
+++ train.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import wandb
+from torch import optim
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+
+from utils.data_loading import BasicDataset, CarvanaDataset
+from utils.dice_score import dice_loss
+from evaluate import evaluate
+from unet import UNet
+
+dir_img = Path('./data/imgs/')
+dir_mask = Path('./data/masks/')
+dir_checkpoint = Path('./checkpoints/')
