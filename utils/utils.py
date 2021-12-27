c92e06efbc3fd1587bb0926a7a8f10774a2e7e40
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2021-12-27 13:34:28 +0000

    Update utils.py
    Added postProcessing and generateInstanceMap

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
-
+from skimage.morphology import remove_small_objects, remove_small_holes, reconstruction, disk
+import warnings
 
 #bb = DefaultConfig.img_rows
 bb = 128 #Nucleus
@@ -127,4 +128,34 @@ def getPatchs(img, clickMap, boundingBoxes, cx, cy, m, n):
     # nucPoints: (total, 1, 128, 128)
     # otherPoints: (total, 1, 128, 128)
     return patchs, nucPoints, otherPoints   
+
+
+#preds(no.patchs, 128, 128), nucPoints(no.patchs, 1, 128, 128) 
+def postProcessing(preds, thresh=0.33, minSize=10, minHole=30, doReconstruction=False, nucPoints=None):
+    masks = preds > thresh
+    masks = remove_small_objects(masks, min_size=minSize)
+    masks = remove_small_holes(masks, area_threshold=minHole)
+    if doReconstruction:
+        for i in range(len(masks)):
+            thisMask = masks[i]
+            # thisMarker = nucPoints[i, :, :, 0] > 0
+            thisMarker = nucPoints[i, 0, :, :] > 0
+            
+            try:
+                thisMask = reconstruction(thisMarker, thisMask, footprint=disk(1))
+                masks[i] = np.array([thisMask])
+            except:
+                warnings.warn('Nuclei reconstruction error #' + str(i))
+    return masks    #masks(no.patchs, 128, 128)
     
+
+
+def generateInstanceMap(masks, boundingBoxes, m, n):
+    instanceMap = np.zeros((m, n), dtype=np.uint16)
+    for i in range(len(masks)):
+        thisBB = boundingBoxes[i]
+        thisMaskPos = np.argwhere(masks[i] > 0)
+        thisMaskPos[:, 0] = thisMaskPos[:, 0] + thisBB[1]
+        thisMaskPos[:, 1] = thisMaskPos[:, 1] + thisBB[0]
+        instanceMap[thisMaskPos[:, 0], thisMaskPos[:, 1]] = i + 1
+    return instanceMap
