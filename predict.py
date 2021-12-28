07c6bf5a62e3e1da39e70181ebe482f16f183b4b
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2021-12-28 18:22:25 +0000

    Update predict.py
    Prediction now working

--- predict.py
+++ predict.py
@@ -7,12 +7,14 @@ import torch
 import torch.nn.functional as F
 from PIL import Image
 from torchvision import transforms
+from skimage.color import label2rgb
+import matplotlib.pyplot as plt
 
 from data.dataset_generator import BasicDataset
-from models import UNet
+from models import UNet, NuClick_NN
 from utils.visualisation import plot_img_and_mask
 from config import DefaultConfig
-from utils.utils import getCoordinatesFromCSV, getClickMapAndBoundingBox, getPatchs, postProcessing
+from utils.utils import getCoordinatesFromCSV, getClickMapAndBoundingBox, getPatchs, postProcessing, generateInstanceMap
 
 
 def predict_img(net,
@@ -25,64 +27,50 @@ def predict_img(net,
 
     #Get click coordinates from CSV file
     cx, cy = getCoordinatesFromCSV(points_csv)
-    #Get the height and width of the image
-    imgHeight, imgWidth = full_img.size
+    imgWidth = full_img.width
+    imgHeight = full_img.height
     #Get click map and bounding box
-    clickmap, boundingbox = getClickMapAndBoundingBox(cx, cy, imgHeight, imgWidth)
+    clickMap, boundingBoxes = getClickMapAndBoundingBox(cx, cy, imgHeight, imgWidth)
 
-    #Convert full_img to numpy array (3,256,256)
-    image = np.asarray(full_img) 
-    image = image[:, :, :3]
+    #Convert full_img to numpy array (3, imgHeight, imgWidth)
+    image = np.asarray(full_img)[:, :, :3]
     image = np.moveaxis(image, 2, 0)
 
     #Generate patchs, inclusion and exlusion maps
-    patchs, nucPoints, otherPoints = getPatchs(image, clickmap, boundingbox, cx, cy, imgHeight, imgWidth)
+    patchs, nucPoints, otherPoints = getPatchs(image, clickMap, boundingBoxes, cx, cy, imgHeight, imgWidth)
     #Divide patchs by 255
     patchs = patchs / 255
 
-    #Concatenate the maps
+    #Concatenate input to model
     input = np.concatenate((patchs, nucPoints, otherPoints), axis=1, dtype=np.float32)
     input = torch.from_numpy(input)
     input.to(device=device, dtype=torch.float32)
 
+    #Predict
     with torch.no_grad():
-        output = net(input) #output shape = (no.patchs, 1, 128, 128)
+        output = net(input) #(no.patchs, 1, 128, 128)
         output = torch.sigmoid(output)
-        output = torch.squeeze(output, 1)   #(no.patchs, 128,128)
+        output = torch.squeeze(output, 1)   #(no.patchs, 128, 128)
         preds = output.numpy()
 
     try:
-        mask = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=True, nucPoints=nucPoints)
+        masks = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=True, nucPoints=nucPoints)
     except:
-        mask = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=False, nucPoints=nucPoints)
+        masks = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=False, nucPoints=nucPoints)
        
-
-        # if net.n_classes > 1:
-        #     probs = F.softmax(output, dim=1)[0]
-        # else:
-        #     probs = torch.sigmoid(output)[0]
-
-    #     tf = transforms.Compose([
-    #         transforms.ToPILImage(),
-    #         transforms.Resize((full_img.size[1], full_img.size[0])),
-    #         transforms.ToTensor()
-    #     ])
-
-    #     full_mask = tf(probs.cpu()).squeeze()
-
-    # if net.n_classes == 1:
-    #     return (full_mask > out_threshold).numpy()
-    # else:
-    #     return F.one_hot(full_mask.argmax(dim=0), net.n_classes).permute(2, 0, 1).numpy()
+    #Generate instanceMap
+    instanceMap = generateInstanceMap(masks, boundingBoxes, imgHeight, imgWidth)
+    return instanceMap
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
-    parser.add_argument('--model', '-m', metavar='FILE', required=True,
+    
+    parser.add_argument('--model', '-m', metavar='PATH', required=True,
                         help='Specify the file in which the model is stored')
-    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', help='Filenames of input images', required=True)
-    parser.add_argument('--points', '-p', metavar='PATH', help='path to a text/csv file containing point location in (x,y) format', required=True)
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+', help='Filenames of output images')
+    parser.add_argument('--input', '-i', metavar='PATH', nargs='+', help='Path to the input images', required=True)
+    parser.add_argument('--points', '-p', metavar='PATH', nargs='+', help='Path to CSV files containing point location in x,y format', required=True)
+    #parser.add_argument('--output', '-o', metavar='PATH', nargs='+', help='Filenames of output images')
     parser.add_argument('--viz', '-v', action='store_true',
                         help='Visualize the images as they are processed')
     parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
@@ -94,27 +82,31 @@ def get_args():
     return parser.parse_args()
 
 
-def get_output_filenames(args):
-    def _generate_name(fn):
-        split = os.path.splitext(fn)
-        return f'{split[0]}_OUT{split[1]}'
-
-    return args.output or list(map(_generate_name, args.input))
+#Generate an output name
+def get_output_filename(fn):
+    split = os.path.splitext(fn)
+    return f'{split[0]}_OUT{split[1]}'
 
 
-def mask_to_image(mask: np.ndarray):
-    if mask.ndim == 2:
-        return Image.fromarray((mask * 255).astype(np.uint8))
-    elif mask.ndim == 3:
-        return Image.fromarray((np.argmax(mask, axis=0) * 255 / mask.shape[0]).astype(np.uint8))
+#Generate a list of tuples (path to image, path to points) from args
+def get_images_points(args):
+    images_points = []
+    if len(args.input) != len(args.points):
+        raise ValueError("Images and points do not match")
+    else:
+        for i in range(len(args.input)):
+            entry = (args.input[i], args.points[i])
+            images_points.append(entry)
+    return images_points
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     args = get_args()
-    in_files = args.input
-    out_files = get_output_filenames(args)
+    images_points = get_images_points(args)
+    # out_files = get_output_filenames(args)
 
-    net = UNet(n_channels=3, n_classes=2)
+    net = NuClick_NN(n_channels=5, n_classes=1)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Loading model {args.model}')
@@ -125,23 +117,29 @@ if __name__ == '__main__':
 
     logging.info('Model loaded!')
 
-    for i, filename in enumerate(in_files):
-        logging.info(f'\nPredicting image {filename} ...')
-        img = Image.open(filename)
+    for i, image_point in enumerate(images_points):
+        imagePath = image_point[0]
+        pointsPath = image_point[1]
+        logging.info(f'\nPredicting image {imagePath} ...')
+        img = Image.open(imagePath)
 
-        mask = predict_img(net=net,
+        instanceMap = predict_img(net=net,
                            full_img=img,
                            scale_factor=args.scale,
                            out_threshold=args.mask_threshold,
-                           points_csv=args.points,
+                           points_csv=pointsPath,
                            device=device)
 
         if not args.no_save:
-            out_filename = out_files[i]
-            result = mask_to_image(mask)
-            result.save(out_filename)
-            logging.info(f'Mask saved to {out_filename}')
+            #Save instance map
+            out_filename = get_output_filename(imagePath)
+            Image.fromarray((instanceMap * 255).astype(np.uint8)).save(out_filename)
+            logging.info(f'Instance map saved')
 
         if args.viz:
-            logging.info(f'Visualizing results for image {filename}, close to continue...')
-            plot_img_and_mask(img, mask)
+            #Visualise instance map
+            logging.info(f'Visualizing results for image {imagePath}, close to continue...')
+            instanceMap_RGB = label2rgb(instanceMap, image=np.asarray(img)[:, :, :3], alpha=0.3, bg_label=0, bg_color=(0, 0, 0), image_alpha=1,kind='overlay')
+            plt.figure(), plt.imshow(instanceMap_RGB)
+            plt.show()
+            
