fc3d778482f699bee8ce64228c5a4e1c0a66fb0c
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2021-12-28 18:21:54 +0000

    Update utils.py
    Tidying up

--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,13 @@ import csv
 from skimage.morphology import remove_small_objects, remove_small_holes, reconstruction, disk
 import warnings
 
-#bb = DefaultConfig.img_rows
-bb = 128 #Nucleus
 
+bb = 128    #128 for nucleus
+
+
+#Returns a clickMap(m,n), and boundingBoxes
+#cx, cy: lists of x and y coordinates
+#m, n: height and width of the full image
 def getClickMapAndBoundingBox(cx, cy, m, n):
     clickMap = np.zeros((m, n), dtype=np.uint8)
 
@@ -45,46 +49,44 @@ def getClickMapAndBoundingBox(cx, cy, m, n):
 
 #Returns a list of x coordinates and a list of y coordinates from the given CSV file
 def getCoordinatesFromCSV(filename):
-   
+    #Open file
     file = open(filename)
     csvReader = csv.reader(file)
+
     clicks_x = []
     clicks_y = []
 
     for row in csvReader:
+        #If the line does not have exactly two values:
         if len(row) != 2:
             raise ValueError(f"The CSV file: '{filename}' does not have valid entries.")
+
+        #Add x and y
         v1 = row[0]
         v2= row[1]
-
         x = int(v1)
         y = int(v2)
-
         clicks_x.append(x)
         clicks_y.append(y)
 
     file.close()
 
+    #If the file is empty:
     if len(clicks_x) == 0:
         raise ValueError(f"The CSV file '{filename}' is empty")
 
     return clicks_x, clicks_y
 
 
+#Returns patchs, nucPoints, otherPoints
 # m: height of img, n: width of img
 def getPatchs(img, clickMap, boundingBoxes, cx, cy, m, n):
     # total = number of clicks
     total = len(boundingBoxes)
-    img = np.array([img])   #img.shape=(1,3,256,256)
-    clickMap = np.array([clickMap])     #clickmap.shape=(1,256,256)
+    img = np.array([img])   #img.shape=(1,3,m,n)
+    clickMap = np.array([clickMap])     #clickmap.shape=(1,m,n)    
+    clickMap = clickMap[:, np.newaxis, ...]    #clickmap.shape=(1,1,m,n)
 
-    #clickMap = clickMap[..., np.newaxis]
-    clickMap = clickMap[:, np.newaxis, ...]    #clickmap.shape=(1,1,256,256)
-
-    # patchs = np.ndarray((total, bb, bb, 3), dtype=np.uint8)
-    # nucPoints = np.ndarray((total, bb, bb, 1), dtype=np.uint8)
-    # otherPoints = np.ndarray((total, bb, bb, 1), dtype=np.uint8)
-    # PyTorch - channel first
     patchs = np.ndarray((total, 3, bb, bb), dtype=np.uint8)
     nucPoints = np.ndarray((total, 1, bb, bb), dtype=np.uint8)
     otherPoints = np.ndarray((total, 1, bb, bb), dtype=np.uint8)
@@ -109,27 +111,23 @@ def getPatchs(img, clickMap, boundingBoxes, cx, cy, m, n):
         xEnd = boundingBox[2]
         yEnd = boundingBox[3]
 
-        #patchs[i] = img[0, yStart:yEnd + 1, xStart:xEnd + 1, :]
         patchs[i] = img[0, :, yStart:yEnd + 1, xStart:xEnd + 1]
 
-        #thisClickMap = np.zeros((1, m, n, 1), dtype=np.uint8)
-        #thisClickMap[0, cy[i], cx[i], 0] = 1
         thisClickMap = np.zeros((1, 1, m, n), dtype=np.uint8)
         thisClickMap[0, 0, cy[i], cx[i]] = 1
 
         othersClickMap = np.uint8((clickMap - thisClickMap) > 0)
 
-        # nucPoints[i] = thisClickMap[0, yStart:yEnd + 1, xStart:xEnd + 1, :]
-        # otherPoints[i] = othersClickMap[0, yStart:yEnd + 1, xStart:xEnd + 1, :]
         nucPoints[i] = thisClickMap[0, :, yStart:yEnd + 1, xStart:xEnd + 1]
         otherPoints[i] = othersClickMap[0, :, yStart:yEnd + 1, xStart:xEnd + 1]
 
-    # patchs: (total, 3, 128, 128)
-    # nucPoints: (total, 1, 128, 128)
-    # otherPoints: (total, 1, 128, 128)
+    # patchs: (total, 3, m, n)
+    # nucPoints: (total, 1, m, n)
+    # otherPoints: (total, 1, m, n)
     return patchs, nucPoints, otherPoints   
 
 
+#Returns masks
 #preds(no.patchs, 128, 128), nucPoints(no.patchs, 1, 128, 128) 
 def postProcessing(preds, thresh=0.33, minSize=10, minHole=30, doReconstruction=False, nucPoints=None):
     masks = preds > thresh
@@ -138,7 +136,6 @@ def postProcessing(preds, thresh=0.33, minSize=10, minHole=30, doReconstruction=
     if doReconstruction:
         for i in range(len(masks)):
             thisMask = masks[i]
-            # thisMarker = nucPoints[i, :, :, 0] > 0
             thisMarker = nucPoints[i, 0, :, :] > 0
             
             try:
@@ -149,9 +146,9 @@ def postProcessing(preds, thresh=0.33, minSize=10, minHole=30, doReconstruction=
     return masks    #masks(no.patchs, 128, 128)
     
 
-
+#Returns instanceMap
 def generateInstanceMap(masks, boundingBoxes, m, n):
-    instanceMap = np.zeros((m, n), dtype=np.uint16)
+    instanceMap = np.zeros((m, n), dtype=np.uint16) 
     for i in range(len(masks)):
         thisBB = boundingBoxes[i]
         thisMaskPos = np.argwhere(masks[i] > 0)
