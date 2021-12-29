c637eba888d9a3de9aad63b016ecb3ba8a9355cf
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2021-12-29 15:38:38 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -15,7 +15,9 @@ from data.dataset_generator import BasicDataset
 from models import UNet, NuClick_NN
 from utils.visualisation import plot_img_and_mask
 from config import DefaultConfig
-from utils.utils import getCoordinatesFromCSV, getClickMapAndBoundingBox, getPatchs, postProcessing, generateInstanceMap
+from utils.process import post_processing, gen_instance_map
+from utils.misc import get_coords_from_csv, get_clickmap_boundingbox, get_output_filename, get_images_points
+from utils.guiding_signals import get_patches_and_signals
 
 
 def predict_img(net,
@@ -27,18 +29,18 @@ def predict_img(net,
     net.eval()
 
     #Get click coordinates from CSV file
-    cx, cy = getCoordinatesFromCSV(points_csv)
+    cx, cy = get_coords_from_csv(points_csv)
     imgWidth = full_img.width
     imgHeight = full_img.height
     #Get click map and bounding box
-    clickMap, boundingBoxes = getClickMapAndBoundingBox(cx, cy, imgHeight, imgWidth)
+    clickMap, boundingBoxes = get_clickmap_boundingbox(cx, cy, imgHeight, imgWidth)
 
     #Convert full_img to numpy array (3, imgHeight, imgWidth)
     image = np.asarray(full_img)[:, :, :3]
     image = np.moveaxis(image, 2, 0)
 
     #Generate patchs, inclusion and exlusion maps
-    patchs, nucPoints, otherPoints = getPatchs(image, clickMap, boundingBoxes, cx, cy, imgHeight, imgWidth)
+    patchs, nucPoints, otherPoints = get_patches_and_signals(image, clickMap, boundingBoxes, cx, cy, imgHeight, imgWidth)
     #Divide patchs by 255
     patchs = patchs / 255
 
@@ -54,31 +56,28 @@ def predict_img(net,
         output = torch.squeeze(output, 1)   #(no.patchs, 128, 128)
         preds = output.numpy()
 
-    try:
-        masks = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=True, nucPoints=nucPoints)
-    except:
-        masks = postProcessing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=False, nucPoints=nucPoints)
-       
+
+    masks = post_processing(preds, thresh=0.5, minSize=10, minHole=30, doReconstruction=True, nucPoints=nucPoints)
+    
     #Generate instanceMap
-    instanceMap = generateInstanceMap(masks, boundingBoxes, imgHeight, imgWidth)
+    instanceMap = gen_instance_map(masks, boundingBoxes, imgHeight, imgWidth)
     return instanceMap
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
 
-    parser.add_argument('--model', '-m', metavar='PATH', required=True,
-                        help='Specify the file in which the model is stored')
-    #parser.add_argument('--input', '-i', metavar='PATH', nargs='+', help='Path to the input images', required=True)
-    #parser.add_argument('--points', '-p', metavar='PATH', nargs='+', help='Path to CSV files containing point location in x,y format', required=True)
+    parser.add_argument('--model', '-m', metavar='NAME', required=True, choices=['NuClick', 'UNet'], help='Name of the model')
+    parser.add_argument('--pretrained_weights', '-w', metavar='PATH', required=True,
+                        help='Path to the pretrained weights')
+    
     imageGroup = parser.add_mutually_exclusive_group(required=True)
     imageGroup.add_argument('--image', '-i', metavar='PATH', nargs='+', help='Path to the input images')
-    imageGroup.add_argument('-imageDir', metavar='PATH', help='Path to the directory containing input images')
+    imageGroup.add_argument('-imgdir', metavar='PATH', help='Path to the directory containing input images')
 
     pointsGroup = parser.add_mutually_exclusive_group(required=True)
-    pointsGroup.add_argument('--points', '-p', metavar='PATH', nargs='+', help='Path to CSV files containing points')
-    pointsGroup.add_argument('-pointsDir', metavar='PATH', help='Path to the directory containing the CSV files')
-    
+    pointsGroup.add_argument('--points', '-p', metavar='PATH', nargs='+', help='Path to the CSV files containing points')
+    pointsGroup.add_argument('-pntdir', metavar='PATH', help='Path to the directory containing the CSV files')
     
     parser.add_argument('--output', '-o', metavar='PATH', help='Directory where the instance maps will be saved into')
     parser.add_argument('--viz', '-v', action='store_true',
@@ -92,67 +91,22 @@ def get_args():
     return parser.parse_args()
 
 
-#Generate an output name
-def get_output_filename(fn, outDir):
-    if outDir is None:
-        split = os.path.splitext(fn)
-        return f'{split[0]}_OUT{split[1]}'
-    else:
-        path = Path(fn)
-        filename = path.stem
-        ext = path.suffix
-        return f'{outDir}/{filename}_OUT{ext}'
-
-
-
-#Generate a list of tuples (path to image, path to points) from args
-def get_images_points(args):
-    images = []
-    points = []
-    images_points = []
-
-    #If image names are provided:
-    if (args.imageDir is None):
-        images = args.image
-    #Else if a directory is provided:
-    else:
-        for fn in os.listdir(args.imageDir):
-            if os.path.splitext(fn)[1] == '.png':
-                images.append(f'{args.imageDir}/{fn}')
-        images.sort()
-
-    if (args.pointsDir is None):
-        points = args.points
-    else:
-        for fn in os.listdir(args.pointsDir):
-            if os.path.splitext(fn)[1] == '.csv':
-                points.append(f'{args.pointsDir}/{fn}')
-        points.sort()
-
-    
-    if (len(images) != len(points)):
-        raise ValueError("Images and points do not match")
-
-    for i in range(len(images)):
-        images_points.append((images[i], points[i]))
-
-    return images_points
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     args = get_args()
     images_points = get_images_points(args)
-    # out_files = get_output_filenames(args)
 
-    net = NuClick_NN(n_channels=5, n_classes=1)
+    if (args.model == 'NuClick'):
+        net = NuClick_NN(n_channels=5, n_classes=1)
+    elif (args.model == 'UNet'):
+        net = UNet(n_channels=5, n_classes=1)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Loading model {args.model}')
     logging.info(f'Using device {device}')
 
     net.to(device=device)
-    net.load_state_dict(torch.load(args.model, map_location=device))
+    net.load_state_dict(torch.load(args.pretrained_weights, map_location=device))
 
     logging.info('Model loaded!')
 
